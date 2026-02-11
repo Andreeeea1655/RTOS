2ab83f6795bb525b29655e350c94418ff5942012
Andreea <166999031+Andreeeea1655@users.noreply.github.com>
2026-02-11 15:51:51 +0200

    final

--- src/rtos_config.h
+++ src/rtos_config.h
@@ -4,8 +4,9 @@
 #define RTOS_TICK_RATE_HZ 1000  // 1 ms
 #define CPU_CLOCK_HZ 48000000   // 48 MHz
 
-#define RTOS_MAX_TASKS 5
+#define RTOS_MAX_TASKS 6
 #define RTOS_MAX_PRIORITIES 32
-#define RTOS_STACK_SIZE 256
+#define RTOS_STACK_SIZE 512
+
 
 #endif
